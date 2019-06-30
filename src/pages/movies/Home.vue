adb71ebd711dc56c68ac6ac4ca9e9e4fd06d5dbd
杨浩然 <bill-billion@outlook.com>
2019-06-30 18:53:42 +0800

    首页

---
+++ src/pages/movies/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Nav></Nav>
+        <Intheaters></Intheaters>
+    </div>
+</template>
+
+<style lang="stylus" scoped>
+
+</style>
+
+<script>
+import Nav from './Nav'
+import Intheaters from './Intheaters'
+export default {
+    components:{
+        Nav,
+        Intheaters
+    }
+}
+</script>
---
+++ src/pages/movies/Intheaters.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Intheaters
+    </div>
+</template>
